f3798ee1d20ebbffa3c7483c20a8052cc3d6b0fa
AntonyChR <antonychr99@gmail.com>
2022-07-07 12:25:55 -0500

    add: loading component

---
+++ src/components/response-viewer/loading/Loading.tsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+import { RequestContext } from '../../../context';
+import classes from './Loading.module.scss';
+
+export const Loading = () => {
+    const {abortRequest} = useContext(RequestContext);
+    return (
+        <div className={classes.wrapper}>
+            <span className={classes.spinner}></span>
+            <span onClick={()=>abortRequest()}>Cancel</span>
+        </div>
+    );
+};
