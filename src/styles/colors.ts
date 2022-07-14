b0e4f7a62a65e9a1421d53dadb6ceaef5da4588a
AntonyChR <antonychr99@gmail.com>
2022-07-13 19:00:42 -0500

    remove unnecessary colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,10 +1,4 @@
 export const colors = {
-    bg: {
-        error: '#160b0b',
-        success: '#0c130d',
-        info: '#071318',
-        warning: '#191207',
-    },
     methods: {
         GET: '#66bb6a',
         POST: '#29b6f6',
